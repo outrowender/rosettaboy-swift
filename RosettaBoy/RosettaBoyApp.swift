e230e43a10ed6c55a3718b852dd2bffa0593a08e
Wender <outrowender@gmail.com>
2023-04-28 15:30:24 -0300

    Initial Commit

---
+++ RosettaBoy/RosettaBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RosettaBoyApp.swift
+//  RosettaBoy
+//
+//  Created by Wender on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RosettaBoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
