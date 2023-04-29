a1c876e934ab76bafef7472c289175161d8f3230
Wender <outrowender@gmail.com>
2023-04-29 14:31:18 -0300

    added flags register logic

--- RosettaBoy/Source/CPU.swift
+++ RosettaBoy/Source/CPU.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
+// MARK: - 8-bit CPU architecture
+
 struct CPU {
     
-    // MARK: - CPU Registers
     var a: UInt8 = 0b00000000
     var b: UInt8 = 0b00000000
     var c: UInt8 = 0b00000000
     var d: UInt8 = 0b00000000
     var e: UInt8 = 0b00000000
-    var f: UInt8 = 0b00000000
-    var g: UInt8 = 0b00000000
+    var f: FRegister = FRegister(0b00000000)
     var h: UInt8 = 0b00000000
     var i: UInt8 = 0b00000000
     
@@ -28,11 +28,11 @@ struct CPU {
 extension CPU {
     
     var af: UInt16 {
-        get { UInt16(UInt16(a) << 8 + UInt16(f)) }
+        get { UInt16(UInt16(a) << 8 + UInt16(f.asUint8)) }
         
         set {
             self.a = UInt8(newValue >> 8 & 0x00ff)
-            self.f = UInt8(newValue & 0x00ff)
+            self.f = FRegister(UInt8(newValue & 0x00ff))
         }
     }
     
@@ -62,4 +62,75 @@ extension CPU {
             self.i = UInt8(newValue & 0x00ff)
         }
     }
+}
+
+// MARK: - CPU Execute
+
+extension CPU {
+    func execute(_ instruction: Instruction){
+        switch instruction {
+        case .ADD(let target):
+            switch target {
+            case .A:
+                print("A")
+            case .B:
+                print("B")
+            case .C:
+                print("C")
+            case .D:
+                print("D")
+            case .E:
+                print("E")
+            case .H:
+                print("H")
+            case .L:
+                print("L")
+            }
+            
+        }
+    }
+}
+
+// MARK: - Custom Flags register
+
+struct FRegister {
+    var zero: Bool = false      // 7
+    var subtract: Bool = false  // 6
+    var halfCarry: Bool = false // 5
+    var carry: Bool = false     // 4
+    
+    init(zero: Bool, subtract: Bool, halfCarry: Bool, carry: Bool) {
+        self.zero = zero
+        self.subtract = subtract
+        self.halfCarry = halfCarry
+        self.carry = carry
+    }
+    
+    init(_ u8: UInt8) {
+        asUint8 = u8
+    }
+    
+    var asUint8: UInt8 {
+        get {
+            UInt8(zero ? 128 : 0)     | //1000 0000
+            UInt8(subtract ? 64 : 0)  | //0100 0000
+            UInt8(halfCarry ? 32 : 0) | //0010 0000
+            UInt8(carry ? 16 : 0)       //0001 0000
+        }
+        
+        set {
+            self.zero = (newValue >> 7) == 1
+            self.subtract = (newValue >> 6) == 1
+            self.halfCarry = (newValue >> 5) == 1
+            self.carry = (newValue >> 4) == 1
+        }
+    }
+}
+
+enum Instruction {
+  case ADD(ArithmeticTarget)
+}
+
+enum ArithmeticTarget {
+    case A, B, C, D, E, H, L
 }
--- RosettaBoyTests/CPUTests.swift
+++ RosettaBoyTests/CPUTests.swift
@@ -18,13 +18,13 @@ final class CPUTests: XCTestCase {
         var cpu = CPU()
         
         // WHEN
-        cpu.af = UInt16(0b1000001100001010)
+        cpu.af = UInt16(0b1000001100100000)
         
         // THEN
         
         XCTAssertEqual(cpu.a, UInt8(0b10000011))
-        XCTAssertEqual(cpu.f, UInt8(0b00001010))
-        XCTAssertEqual(cpu.af, UInt16(0b1000001100001010))
+        XCTAssertEqual(cpu.f.asUint8, UInt8(0b00100000))
+        XCTAssertEqual(cpu.af, UInt16(0b1000001100100000))
     }
     
     func testVirtualRegistersMustDoBC() throws {
@@ -72,4 +72,36 @@ final class CPUTests: XCTestCase {
         XCTAssertEqual(cpu.hi, UInt16(0b1000001100001010))
     }
     
+    func testFlagsShouldConvertToBoolAndBackCorrectly() throws {
+        // GIVEN
+        let cpu = FRegister(0b01000000)
+        
+        // THEN
+        XCTAssertTrue(cpu.subtract)
+        XCTAssertFalse(cpu.zero || cpu.halfCarry || cpu.carry)
+        XCTAssertEqual(cpu.asUint8, 0b01000000)
+    }
+    
+    func testFlagsShouldInitializeWithBit() throws {
+        // GIVEN
+        let cpu = FRegister(
+            zero: true,
+            subtract: false,
+            halfCarry: false,
+            carry: true)
+        
+        // THEN
+        XCTAssertTrue(cpu.zero && cpu.carry)
+        XCTAssertEqual(cpu.asUint8, 0b10010000)
+    }
+    
+    func testFlagsShouldAlwaysClearFirst4bits() throws {
+        // GIVEN
+        let cpu = FRegister(0b01001111)
+        
+        // THEN
+        XCTAssertTrue(cpu.subtract)
+        XCTAssertFalse(cpu.zero || cpu.halfCarry || cpu.carry)
+        XCTAssertEqual(cpu.asUint8, 0b01000000)
+    }
 }
